979de09e668df40e892032ab506e38d7efc5a483
jordi <jhidalgo87@gmail.com>
2015-06-07 19:40:36 +0200

    Timer working at right rate

--- src/stm32f1xx_hal_msp.c
+++ src/stm32f1xx_hal_msp.c
@@ -74,7 +74,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim)
   
   /*##-2- Configure the NVIC for TIMx ########################################*/
   /* Set the TIMx priority */
-  HAL_NVIC_SetPriority(TIMx_IRQn, 3, 0);
+  HAL_NVIC_SetPriority(TIMx_IRQn, 4, 0);
 
   /* Enable the TIMx global Interrupt */
   HAL_NVIC_EnableIRQ(TIMx_IRQn);
