ba9854d4fb2f06eed3734c2f561499dcab344d9d
jordi <jhidalgo87@gmail.com>
2017-04-12 00:29:59 +0200

    Oscillator test working

--- tst/oscillator_tst/oscillator_tst.cc
+++ tst/oscillator_tst/oscillator_tst.cc
@@ -26,13 +26,15 @@ This file is part of XXXXXXX
 
 #include <stdio.h>
 
-
+/**
+ * Wait usart interface until is ready
+ */
 void wait_usart_ready(void){
 
-	// Wait for value 1( ASCII 49) as uart confirmation
+	/** Wait for value 1( ASCII 49) as uart confirmation  */
 	while( USART_ReceiveData(USART2) != 49)
 
-	// Debug usart confirmation
+	/** Debug usart confirmation  */
 	if(0){
 		iprintf("Received data = ");
 		uint16_t uart_in = USART_ReceiveData(USART2);
@@ -40,18 +42,29 @@ void wait_usart_ready(void){
 		iprintf("\n");
 	}
 
-	// Ready to start test
+	/** Ready to start test  */
     iprintf("\nTEST:  Oscillator\n-----------------------");
 
 }
 
+/**
+ * Convert integers to char strings
+ * @param num an integer
+ */
 void intNum2CharStr(int32_t num){
 	static char string[11];
 	sprintf(string, "%d", num);
 	iprintf((const char*)string);
 }
 
+/**
+ * Size of reference and output buffers
+ */
 #define BUFF_SIZE 256
+
+/**
+ * Sine unit test reference buffer
+ */
 int32_t buff_sin_ref[BUFF_SIZE] = {
 		1792,3840,5888,8192,9984,12032,14080,15872,17664,19456,21248,22784,24320,25600,26880,27904,28928,29696,30720,31232,31744,32000,32256,
 		32256,32256,32000,31744,31232,30720,29696,28928,27904,26880,25600,24320,22784,21248,19456,17920,16128,14336,12288,10240,8192,6144,4096,
@@ -67,6 +80,9 @@ int32_t buff_sin_ref[BUFF_SIZE] = {
 		-14592,-16384,-18176,-19968,-21760,-23296,-24832,-26112,-27392,-28416
 };
 
+/**
+ * Square unit test reference buffer
+ */
 int32_t buff_squ_ref[BUFF_SIZE] = {
 		32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,32256,
 		32256,32256,32256,32256,32256,32256,32256,32512,-32768,-32768,-32768,-32768,-32768,-32768,-32768,-32768,-32768,-32768,-32768,-32768,-32768,-32768,-32768,
@@ -82,6 +98,9 @@ int32_t buff_squ_ref[BUFF_SIZE] = {
 
 };
 
+/**
+ * Saw unit test reference buffer
+ */
 int32_t buff_saw_ref[BUFF_SIZE] = {
 		-10496,-9728,-8960,-8448,-7680,-6912,-6400,-5632,-4864,-4352,-3584,-2816,-2304,-1536,-768,-256,512,1280,1792,2560,3328,3840,4608,5376,5888,6656,
 		7424,7936,8704,9472,9984,10752,11520,12032,12800,13568,14080,14848,15616,16128,16896,17664,18176,18944,19712,20224,20992,21760,22272,23040,
@@ -96,91 +115,123 @@ int32_t buff_saw_ref[BUFF_SIZE] = {
 		25088,25856,26368,27136,27904,28416,29184,29952,30464,31232,32000,0,
 };
 
-// Structure instances
+/**
+ * Structure holding the main synth parameters
+ */
 synth_params_t synth_params;
 
+/**
+ * Oscillator class instance
+ */
 Oscillator osc;
 
-int32_t sample;
-
-
-
-
+/**
+ * Sine unit test output buffer
+ */
 int32_t buff_sin_out [BUFF_SIZE];
+
+/**
+ * Square unit test output buffer
+ */
 int32_t buff_squ_out [BUFF_SIZE];
+
+/**
+ * Saw unit test output buffer
+ */
 int32_t buff_saw_out [BUFF_SIZE];
 
+/**
+ * Sine oscillator unit test
+ */
 void test_sine_out(void){
 
-	// Configure oscillator 1
+	int32_t sample;
+
+	/** Configure oscillator 1 */
 	osc_shape_t shape_osc1 = SIN;
 	osc.set_shape(shape_osc1);
 	osc.setFreqFrac(1000);
 
+	/** Get oscillator samples */
 	for(int i=0; i<BUFF_SIZE; i++){
 		sample =  osc.compute_osc(&synth_params);
 
 		buff_sin_out[i] = sample;
 	}
 
-	iprintf("]\nbuff_sin_out = [");
+	/** Print output buffer */
+	iprintf("\nbuff_sin_out = [");
 	for(int j=0; j<BUFF_SIZE;  j++ ){
 		intNum2CharStr(buff_sin_out[j]);
 		iprintf(",");
 	}
 	iprintf("]\n");
 
-
+	/** Compare output vs reference */
 	TEST_ASSERT_EQUAL_INT32_ARRAY(buff_sin_ref,buff_sin_out,BUFF_SIZE);
 
 }
 
+/**
+ * Square oscillator unit test
+ */
 void test_square_out(void){
 
-	// Configure oscillator 1
+	int32_t sample;
+
+	/** Configure oscillator 1  */
 	osc_shape_t shape_osc1 = SQU;
 	osc.set_shape(shape_osc1);
 	osc.setFreqFrac(1000);
 
+	/** Get oscillator samples */
 	for(int i=0; i<BUFF_SIZE; i++){
 		sample =  osc.compute_osc(&synth_params);
 
 		buff_squ_out[i] = sample;
 	}
 
-	iprintf("]\nbuff_squ_out = [");
+	/** Print output buffer */
+	iprintf("\nbuff_squ_out = [");
 	for(int j=0; j<BUFF_SIZE;  j++ ){
 		intNum2CharStr(buff_squ_out[j]);
 		iprintf(",");
 	}
 	iprintf("]\n");
 
-
+	/** Compare output vs reference */
 	TEST_ASSERT_EQUAL_INT32_ARRAY(buff_squ_ref,buff_squ_out,BUFF_SIZE);
 
 }
 
+/**
+ * Saw oscillator unit test
+ */
 void test_saw_out(void){
 
-	// Configure oscillator 1
+	int32_t sample;
+
+	/** Configure oscillator 1  */
 	osc_shape_t shape_osc1 = SAW;
 	osc.set_shape(shape_osc1);
 	osc.setFreqFrac(1000);
 
+	/** Get oscillator samples */
 	for(int i=0; i<BUFF_SIZE; i++){
 		sample =  osc.compute_osc(&synth_params);
 
 		buff_saw_out[i] = sample;
 	}
 
-	iprintf("]\nbuff_saw_out = [");
+	/** Print output buffer */
+	iprintf("\nbuff_saw_out = [");
 	for(int j=0; j<BUFF_SIZE;  j++ ){
 		intNum2CharStr(buff_saw_out[j]);
 		iprintf(",");
 	}
 	iprintf("]\n");
 
-
+	/** Compare output vs reference */
 	TEST_ASSERT_EQUAL_INT32_ARRAY(buff_saw_ref,buff_saw_out,BUFF_SIZE);
 
 }
@@ -188,33 +239,35 @@ void test_saw_out(void){
 int main(void)
 {
 
+	/** System init */
 	RCC_ClocksTypeDef RCC_Clocks;
-
 	SystemInit();
 	RCC_Clocks_Init();
 	SystemCoreClockUpdate();
 
-	/* SysTick end of count event each 1ms */
+	/** SysTick end of count event each 1ms */
 	RCC_GetClocksFreq(&RCC_Clocks);
 	SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000);
 
-    // COnfigure and init peripherals
+    /** Configure and init peripherals  */
 	GPIO_Conf_Init();
 	USART_Conf_Init();
 
-    // turn off buffers, so IO occurs immediately
+    /** Turn off buffers, so IO occurs immediately  */
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
 
-    // Wait usart confirmation to start the test
+    /** Wait usart confirmation to start the test  */
     wait_usart_ready();
 
+    /** Start unity and trigger tests */
     UNITY_BEGIN();
     RUN_TEST(test_sine_out);
     RUN_TEST(test_square_out);
     RUN_TEST(test_saw_out);
 
+    /** FInish unity */
     return UNITY_END();
 }
 
