87972c38fb53d77a1d249b81f7685b5e92ca3a99
jordi <jhidalgo87@gmail.com>
2017-05-09 22:33:45 +0200

    Started app from scratch

---
+++ tst/tst_utils/tst_settings.h
@@ -0,0 +1,32 @@
+/*
+@file tst_settings.h
+
+@brief Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+@ Created by Jordi Hidalgo, Ratatech, May 7, 2017
+This file is part of XXXXXXX
+
+    XXXXXXX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XXXXXXX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XXXXXXX.  If not, see <http://www.gnu.org/licenses/>
+*/
+#ifndef TST_TST_UTILS_TST_SETTINGS_H_
+#define TST_TST_UTILS_TST_SETTINGS_H_
+
+#define USART_TST USART3
+
+#define STDOUT_USART 3
+#define STDERR_USART 3
+#define STDIN_USART 3
+
+
+#endif /* TST_TST_UTILS_TST_SETTINGS_H_ */
